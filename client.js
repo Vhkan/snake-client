30e3c018605133293c1f83aa0cbd82119856022d
Vhkan <vhkan@yahoo.com>
2023-09-27 20:26:37 -0600

    Modifying file play.js and adding client.js

---
+++ client.js
@@ -0,0 +1,28 @@
+const { isModuleNamespaceObject } = require("util/types");
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.27.239.219",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected!");
+  });
+  
+  conn.on("data", () => {
+    console.log("you ded cuz you idled");
+  });
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,26 +1,2 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.27.239.219",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected!");
-  });
-  
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
-  });
-  
-  return conn;
-};
-
-
-console.log("Connecting ...");
-connect();
+const { connect } = require('./client');
