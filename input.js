effb7c623eb8dbb23e01b35b770294b7ec4df732
Vhkan <vhkan@yahoo.com>
2023-09-27 22:14:20 -0600

    Adding input.js file

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+setupInput();
+
+module.exports = {setupInput, handleUserInput};
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const net = require("net");
 const { connect } = require('./client');
+const { setupInput, handleUserInput } = require('./input');
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-};
+console.log("Connecting ...");
+connect();
 setupInput();
